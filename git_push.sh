4a9eddeb978524ab443b2309dcd77c8c5b8e2bfc
Goldenprince8420 <rahulgolder0202@gmail.com>
2022-10-05 14:20:03 +0530

    first commit

--- git_push.sh
+++ git_push.sh
@@ -2,5 +2,7 @@ git init
 git add README.md  App Credit_Card_Balance.png     PieChart.png               installer.sh       results_filtered.csv Installments_Payments.csv   Previous_Application.csv   lgbmodel.pkl       scrape_work.py P7_1_PYTHON.ipynb           fastapi                    main.py            streamlit_app.py    CreditAmount.png                              P7_2_APP.py                 get_data.sh                requirements.txt  Credit_Card_Balance.csv                       P7_3_PRENSENTATION.pptx     git_push.sh                train_data_domain_filtered.csv
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/Goldenprince8420/CSX_Project1.git
+git remote add origin https://Goldenprince8420:Rahul@8420/CSX_Project1.git
 git push -u origin main --force
+
+
